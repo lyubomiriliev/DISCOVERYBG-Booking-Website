04fef0477c7cbfb800738103a6e10809dc9cb9f0
lyubomiriliev <iliev.lyubomir98@gmail.com>
2024-07-05 15:57:50 +0300

    Added react router dom, pages and components

---
+++ client/src/pages/SiteDetails.jsx
@@ -0,0 +1,9 @@
+const SiteDetails = () => {
+    return (
+        <div>
+            <h1>Site Details</h1>
+        </div>
+    )
+}
+
+export default SiteDetails
---
+++ client/src/pages/TouristSite.jsx
@@ -0,0 +1,9 @@
+const TouristSite = () => {
+    return (
+        <div>
+            <h1>Tourist Site:</h1>
+        </div>
+    )
+}
+
+export default TouristSite
