e1efb545581edbb65449e7bcc17e8d0ee1f0859f
lyubomiriliev <iliev.lyubomir98@gmail.com>
2024-09-23 17:57:45 +0300

    fixed header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,17 @@
-import { Link } from "react-router-dom"
-import { BGflag, discoveryToShare, ENGflag, MTlogo, PLflag, siteLogo } from "../assets"
-import { useTranslation } from "react-i18next"
+import { Link } from "react-router-dom";
+import { BGflag, discoveryToShare, ENGflag, MTlogo, PLflag, siteLogo } from "../assets";
+import { useTranslation } from "react-i18next";
 import { useEffect, useRef, useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
-
     const { i18n } = useTranslation();
     const languageRef = useRef(null);
+    const mobileHeaderRef = useRef(null);
     const { t } = useTranslation();
 
     const [showLanguage, setShowLanguage] = useState(false);
-    const [showLanguageText, setShowLanguageText] = useState(false);
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
     const getFlagForLanguage = (lang) => {
@@ -26,20 +25,30 @@ const Header = () => {
             default:
                 return ENGflag;
         }
-    }
+    };
 
-    const currentFlag = getFlagForLanguage(i18n.language)
+    const currentFlag = getFlagForLanguage(i18n.language);
 
     const changeLanguage = (lang) => {
         i18n.changeLanguage(lang);
         setShowLanguage(false);
         setIsMobileMenuOpen(false);
-    }
+    };
+
+    const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
+
+    const handleMouseEnter = () => {
+        setShowLanguage(true);
+    };
+
+    const handleMouseLeave = () => {
+        setShowLanguage(false);
+    };
+
 
     const handleClickOutside = (event) => {
-        if (languageRef.current && !languageRef.current.contains(event.target)) {
-            setShowLanguage(false);
-            setShowLanguageText(false);
+        if (mobileHeaderRef.current && !mobileHeaderRef.current.contains(event.target)) {
+            setIsMobileMenuOpen(false);
         }
     };
 
@@ -49,19 +58,6 @@ const Header = () => {
             document.removeEventListener("mousedown", handleClickOutside);
         };
     }, [])
-
-    const handleLanguageHover = () => {
-        setShowLanguageText(true);
-        setShowLanguage(true);
-    };
-
-    const handleLanguageLeave = () => {
-        setShowLanguageText(false);
-        setShowLanguage(false);
-    };
-
-
-    const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen)
 
     return (
         <div className="w-full h-24 md:h-40 bg-white shadow-md fixed top-0 left-0 z-50 flex flex-col px-4 md:px-8 items-center">
@@ -79,15 +75,14 @@ const Header = () => {
                 </div>
                 {isMobileMenuOpen ? (
                     <AiOutlineClose className="text-3xl cursor-pointer" onClick={toggleMobileMenu} />
-                ) :
-                    (
-                        <GiHamburgerMenu onClick={toggleMobileMenu} className="text-2xl md:hidden cursor-pointer" />
-                    )
-                }
+                ) : (
+                    <GiHamburgerMenu onClick={toggleMobileMenu} className="text-2xl md:hidden cursor-pointer" />
+                )}
             </div>
+
             {/* MOBILE MENU */}
             {isMobileMenuOpen && (
-                <div className="absolute top-24 left-0 w-full h-60 px-4 py-4 space-y-6 bg-white flex flex-col z-50 drop-shadow-lg">
+                <div ref={mobileHeaderRef} className="absolute top-24 left-0 w-full h-60 px-4 py-4 space-y-6 bg-white flex flex-col z-50 drop-shadow-lg">
                     <Link to="/" onClick={toggleMobileMenu}>
                         <h2 className="font-body uppercase text-black hover:text-secondary duration-300">{t('header.home')}</h2>
                     </Link>
@@ -123,39 +118,37 @@ const Header = () => {
                         <h2 className="font-body uppercase text-black text-sm hover:text-secondary duration-300 font-bold">{t('header.packages')}</h2>
                     </Link>
                 </nav>
-                <div className="flex justify-around items-center">
-                    <div className="relative">
-                        <button
-                            className="font-body uppercase text-black hover:text-secondary duration-300 font-bold"
-                            onMouseEnter={handleLanguageHover}
-                            onMouseLeave={handleLanguageLeave}
-                        >
-                            <img className="w-10 h-auto object-cover" src={currentFlag} alt={i18n.language} />
-                        </button>
+                <div
+                    className="relative"
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <button className="font-body uppercase text-black hover:text-secondary duration-300 font-bold">
+                        <img className="w-10 h-auto object-cover" src={currentFlag} alt={i18n.language} />
+                    </button>
 
-                        {showLanguageText && (
-                            <h3 className="text-sm font-bold text-primary font-body">
-                                {i18n.language.toUpperCase()}
-                            </h3>
-                        )}
-                        {showLanguage && (
-                            <div ref={languageRef} className="absolute left-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                <div className="flex flex-col items-center space-y-2">
+                    {showLanguage && (
+                        <div ref={languageRef} className="absolute right-0 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="flex flex-col items-center space-y-2 bg-gray-200 px-2 py-2 w-20">
+                                <div className="flex gap-2">
+                                    <img className="w-8 h-auto cursor-pointer" src={BGflag} onClick={() => changeLanguage('bg')} alt="EN" />
+                                    <h3 className="text-sm font-bold text-primary font-body">EN</h3>
+                                </div>
+                                <div className="flex gap-2">
                                     <img className="w-8 h-auto cursor-pointer" src={ENGflag} onClick={() => changeLanguage('en')} alt="EN" />
-                                    <img className="w-8 h-auto cursor-pointer" src={BGflag} onClick={() => changeLanguage('bg')} alt="BG" />
-                                    <img className="w-8 h-auto cursor-pointer" src={PLflag} onClick={() => changeLanguage('pl')} alt="PL" />
+                                    <h3 className="text-sm font-bold text-primary font-body">BG</h3>
+                                </div>
+                                <div className="flex gap-2">
+                                    <img className="w-8 h-auto cursor-pointer" src={PLflag} onClick={() => changeLanguage('pl')} alt="EN" />
+                                    <h3 className="text-sm font-bold text-primary font-body">PL</h3>
                                 </div>
                             </div>
-                        )}
-                        {showLanguageText && (
-                            <h3 className="text-sm font-bold text-primary font-body">BG</h3>
-                        )}
-                    </div>
+                        </div>
+                    )}
                 </div>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
